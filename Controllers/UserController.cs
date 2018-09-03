7b9cbefc31a8c1366a135b52868ebf22a581735e
Tom Pullen <tpullen21@gmail.com>
2018-09-03 14:32:25 +0100

    Added API to get user claims

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Claim = B2C_WebApi.Models.Claim;
+
+namespace B2C_WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET: api/User
+        [HttpGet]
+        public string Get()
+        {
+            var userClaims = User.Claims;
+            List<Claim> claims = new List<Claim>();
+            foreach (var userClaim in userClaims)
+            {
+                claims.Add(new Claim
+                {
+                    name = userClaim.Type,
+                    value = userClaim.Value
+                });
+            }
+            var claimsJson = Newtonsoft.Json.JsonConvert.SerializeObject(claims);
+            return claimsJson;
+        }
+    }
+}
---
+++ Models/Claim.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B2C_WebApi.Models
+{
+    public class Claim
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
